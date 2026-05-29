520500d9abae4cbf9c0277d259070eb442230656
Harshavardhan JM <pes1pg25ca371@stu.pes.edu>
2026-05-29 13:24:49 +0530

    Update index.js

--- server/store/index.js
+++ server/store/index.js
@@ -2,15 +2,13 @@ import bcrypt from "bcryptjs";
 import { FileStore } from "./fileStore.js";
 import { PostgresStore } from "./postgresStore.js";
 
-export async function createStore() {
-  const store = process.env.DATABASE_URL
+const store = process.env.DATABASE_URL
     ? new PostgresStore(process.env.DATABASE_URL)
     : new FileStore();
   await store.init();
   store.verifyPassword = verifyPassword;
   return store;
 }
-
 export async function hashPassword(password) {
   return bcrypt.hash(password, 10);
 }
